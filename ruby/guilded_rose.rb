a598ed0a0f96b89f897dd7ed2eb1270475bb882e
javs1 <59986245+javs1@users.noreply.github.com>
2021-12-07 18:41:23 -0600

    Rubycritic minimun required 85.21
    - new methods quality_rules
    - new methods sell_in_rules
    - methods call in update_quality
    
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: Alanjosue97 <perezaln.09@gmail.com>

--- ruby/guilded_rose.rb
+++ ruby/guilded_rose.rb
@@ -4,6 +4,7 @@
 class GildedRose
   def initialize(items)
     @items = items
+    @max_quality = 50
   end
 
   def update_quality
@@ -11,33 +12,45 @@ class GildedRose
       name = item.name
       quality = item.quality
       sell_in = item.sell_in
-      max_quality = 50
-      if (name != 'Aged Brie') && (name != 'Backstage passes to a TAFKAL80ETC concert')
-        quality -= 1 if quality.positive? && (name != 'Sulfuras, Hand of Ragnaros')
-        
-      elsif quality < max_quality
-        quality += 1
-        if name == 'Backstage passes to a TAFKAL80ETC concert'
-          quality += 1 if sell_in < 11 && (quality < max_quality)
-          quality += 1 if sell_in < 6 && (quality < max_quality)
-        end
-      end
+      next unless name != 'Sulfuras, Hand of Ragnaros'
 
-      sell_in -= 1 if name != 'Sulfuras, Hand of Ragnaros'
-
+      # call quality_rules
+      # quality=quality_rules
+      quality = quality_rules(name, quality, sell_in)
+      # call sell_in_rules
+      sell_in -= 1
       if sell_in.negative?
-        if name != 'Aged Brie'
-          if name != 'Backstage passes to a TAFKAL80ETC concert'
-            quality -= 1 if quality.positive? && (name != 'Sulfuras, Hand of Ragnaros')
-          else
-            quality -= quality
-          end
-        elsif quality < max_quality
-          quality += 1
-        end
+        sell_in = sell_in_rules(name, quality, sell_in)
+      else
+        item.quality = quality
       end
-      item.quality = quality
       item.sell_in = sell_in
+    end
+  end
+
+  def quality_rules(name, quality, sell_in)
+    if (name != 'Aged Brie') && (name != 'Backstage passes to a TAFKAL80ETC concert')
+      quality -= 1 if quality.positive?
+
+    elsif quality < @max_quality
+      quality += 1
+      if name == 'Backstage passes to a TAFKAL80ETC concert'
+        quality += 1 if sell_in < 11 && (quality < @max_quality)
+        quality += 1 if sell_in < 6 && (quality < @max_quality)
+      end
+    end
+    quality
+  end
+
+  def sell_in_rules(name, quality, _sell_in)
+    if name != 'Aged Brie'
+      if name != 'Backstage passes to a TAFKAL80ETC concert'
+        quality -= 1 if quality.positive?
+      else
+        quality -= quality
+      end
+    elsif quality < @max_quality
+      quality += 1
     end
   end
 end
